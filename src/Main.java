9612308bf80978b81b3a9cb8d35285340ec36daf
Dina18013 <99754434+Dina18013@users.noreply.github.com>
2022-03-25 23:27:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         
-        int cenaTicket = 30_000; // стоимость билета
-        int bonusMila = 20; // количество рублей для одной бонусной мили
+        int priceTicket = 30_000; // стоимость билета
+        int bonusMiles = 20; // количество рублей для одной бонусной мили
 
 
      
-        int mila = cenaTicket / bonusMila; //расчет количества бонусных миль
-        System.out.println("количество бонусных миль=" + mila);
+        int miles = priceTicket / bonusMiles; //расчет количества бонусных миль
+        System.out.println("количество бонусных миль=" + miles);
     }
 
 }
